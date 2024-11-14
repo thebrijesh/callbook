6a77fa749e4859f8a19dacff048ef8a2b45b9686
brijesh <ibrijeshshingala@gmail.com>
2024-11-14 16:59:21 +0530

    Initial commit

---
+++ src/main/java/com/scm/callbook/CallbookApplication.java
@@ -0,0 +1,13 @@
+package com.scm.callbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scm/callbook/CallbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.callbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
